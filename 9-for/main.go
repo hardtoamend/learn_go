b779909869e647a199fcf99ff627234e9ef9bdfa
hardtoamend <hardtoamen@gmail.com>
2023-03-04 21:53:40 +0800

    Add array.md
    Signed-off-by: hardtoamend <hardtoamen@gmail.com>

---
+++ 9-for/main.go
@@ -0,0 +1,44 @@
+package main
+
+// for 循环
+func main() {
+	//// 1. for 循环基本写法
+	//for i := 0; i < 10; i++ {
+	//	fmt.Println(i)
+	//}
+
+	//// 2. 省略初识语句 ,  但是必须保留初识语句后面的分号;
+	//var i = 0
+	//for ; i < 10; i++ {
+	//	fmt.Println(i)
+	//}
+
+	//// 3. 省略初识语句和结束语句
+	//var i = 10
+	//for i > 0 {
+	//	fmt.Println(i)
+	//	i--
+	//}
+
+	// 4. 无限循环
+	//for {
+	//	fmt.Println("hello")
+	//}
+
+	//// 5. break 跳出循环
+	//for i := 0; i < 5; i++ {
+	//	fmt.Println(i)
+	//	if i == 3 {
+	//		break // 0 1 2 3
+	//	}
+	//}
+
+	//// 6. continue 继续下一次循环
+	//for i := 0; i < 5; i++ {
+	//	if i == 3 {
+	//		continue		// 跳过继续下一次循环
+	//	}
+	//	fmt.Printf("%d", i)
+	//}
+
+}
